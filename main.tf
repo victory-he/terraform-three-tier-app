4bcdee783ff4093d6edc136f9c510ec0ca1786ec
Victor He <72237812+victory-he@users.noreply.github.com>
2022-02-09 00:37:14 -0500

    Fix IGW name

--- main.tf
+++ main.tf
@@ -103,7 +103,7 @@ resource "aws_route_table" "tf-web-rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.tf-igw.id
   }
 
   tags = {
