8da03609a006ce58855ac0bc9e4fb0044c47db38
Victor He <72237812+victory-he@users.noreply.github.com>
2022-02-09 00:33:12 -0500

    Add RDS instance

--- main.tf
+++ main.tf
@@ -274,3 +274,26 @@ resource "aws_lb_listener" "external-elb" {
     target_group_arn = aws_lb_target_group.external-elb.arn
   }
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.default.id
+  engine                 = "mysql"
+  engine_version         = "8.0.20"
+  instance_class         = "db.t2.micro"
+  multi_az               = true
+  name                   = "mydb"
+  username               = "username"
+  password               = "password"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.tf-db-subnet-1.id, aws_subnet.tf-db-subnet-2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
