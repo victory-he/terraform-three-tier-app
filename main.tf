b683e2bb83a9af2797a629e28a88c6f56c877f84
Victor He <72237812+victory-he@users.noreply.github.com>
2022-02-09 00:26:04 -0500

    Add EC2s and SGs

--- main.tf
+++ main.tf
@@ -121,3 +121,111 @@ resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.tf-web-subnet-2.id
   route_table_id = aws_route_table.tf-web-rt.id
 }
+
+#Create EC2 Instance
+resource "aws_instance" "tf-webserver1" {
+  ami                    = "ami-033b95fb8079dc481"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1a"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.tf-web-subnet-1.id
+  user_data              = file("install_apache.sh")
+
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
+resource "aws_instance" "webserver2" {
+  ami                    = "ami-033b95fb8079dc481"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-1b"
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.tf-web-subnet-2.id
+  user_data              = file("install_apache.sh")
+
+  tags = {
+    Name = "Web Server"
+  }
+
+}
+
+# Create Web Security Group
+resource "aws_security_group" "web-sg" {
+  name        = "Web-SG"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.my-tf-vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-SG"
+  }
+}
+
+# Create Web Server Security Group
+resource "aws_security_group" "webserver-sg" {
+  name        = "Webserver-SG"
+  description = "Allow inbound traffic from ALB"
+  vpc_id      = aws_vpc.my-tf-vpc.id
+
+  ingress {
+    description     = "Allow traffic from web layer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web-sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Webserver-SG"
+  }
+}
+
+# Create Database Security Group
+resource "aws_security_group" "database-sg" {
+  name        = "Database-SG"
+  description = "Allow inbound traffic from application layer"
+  vpc_id      = aws_vpc.my-tf-vpc.id
+
+  ingress {
+    description     = "Allow traffic from application layer"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.webserver-sg.id]
+  }
+
+  egress {
+    from_port   = 32768
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Database-SG"
+  }
+}
+
